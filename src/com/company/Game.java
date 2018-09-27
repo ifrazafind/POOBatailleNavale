791ef1978f1be80aa9f43dc4ec4e8f144ac612cc
ifrazafind <35964748+ifrazafind@users.noreply.github.com>
2018-09-27 18:07:25 +0300

    Affichage (Grille) et jouabilité

--- src/com/company/Game.java
+++ src/com/company/Game.java
@@ -1,24 +1,59 @@
 package com.company;
-import java.util.*;
+
+import java.util.concurrent.TimeUnit;
+
 
 public class Game {
 
-	public static void initialize() {
+    public static void initialize() {
 
 	}
 	
-	/*public static void tour(Joueur j) {
-		System.out.println(j.getBateaux().elements());
-		for (Bateau b : j.getBateaux().elements().) {
-			if
-		}
-		if(j.getBateaux().isEmpty());
-	}*/
-	
-	
-    public static void main(String[] args) {
-        Joueur J1 = new Joueur();
-        Joueur J2 = new Joueur();      
+    public static void main(String[] args) throws InterruptedException {
+
+        Joueur joueur1 = new Joueur();
+        Joueur joueur2 = new Joueur();
+
+        Joueur tableauJoueur[] = {joueur1, joueur2};
+
+        for (int i = 0; i<tableauJoueur.length;i++) {
+
+            System.out.print("Joueur " + i + "\n");
+
+            tableauJoueur[i].montrergrille();
+
+            System.out.print("Placer PorteAvion\n");
+            PorteAvion p = new PorteAvion(tableauJoueur[i].lignebateau(), tableauJoueur[i].colonebateau(), tableauJoueur[i].orienterbateau());
+            tableauJoueur[i].ajouterbateau(p);
+            tableauJoueur[i].montrergrille();
+
+            System.out.print("Placer Croiseur\n");
+            Croiseur c = new Croiseur(tableauJoueur[i].lignebateau(), tableauJoueur[i].colonebateau(), tableauJoueur[i].orienterbateau());
+            tableauJoueur[i].ajouterbateau(c);
+            tableauJoueur[i].montrergrille();
+
+            System.out.print("Placer ContreTorpilleur\n");
+            ContreTorpilleur ct = new ContreTorpilleur(tableauJoueur[i].lignebateau(), tableauJoueur[i].colonebateau(), tableauJoueur[i].orienterbateau());
+            tableauJoueur[i].ajouterbateau(ct);
+            tableauJoueur[i].montrergrille();
+
+            System.out.print("Placer SousMarin\n");
+            SousMarin s = new SousMarin(tableauJoueur[i].lignebateau(), tableauJoueur[i].colonebateau(), tableauJoueur[i].orienterbateau());
+            tableauJoueur[i].ajouterbateau(s);
+            tableauJoueur[i].montrergrille();
+
+            System.out.print("Placer Torpilleur\n");
+            Torpilleur t = new Torpilleur(tableauJoueur[i].lignebateau(), tableauJoueur[i].colonebateau(), tableauJoueur[i].orienterbateau());
+            tableauJoueur[i].ajouterbateau(t);
+            tableauJoueur[i].montrergrille();
+
+
+            TimeUnit.SECONDS.sleep(2);
+
+        /*Joueur J1 = new Joueur();
+        Joueur J2 = new Joueur();
+        System.out.println("Type de bateaux : ");
+        System.out.println(" 1 : ContreTorpilleur \n 2 : Croiseur \n 3 : Porte Avion \n 4 : Sous-Marin \n 5 : Torpilleur \n");
         Croiseur c1 = new Croiseur(2, 3, true);
         Torpilleur t1 = new Torpilleur(5, 8, false);
         SousMarin s1 = new SousMarin(3,6,false);
@@ -34,14 +69,14 @@ public class Game {
         //tour(J1);
         
         J1.tir(2,3,J2);
-        System.out.println(J1);
-        c1.avancer(2, 3);
+        System.out.println(J2);
+        c1.avancer(3, 4);
         System.out.println("_____________");
-        System.out.println(J1);
-       
-        
-        //J1.getBateaux().el
+        System.out.println(J1);*/
 
 
+            //J1.getBateaux().el
+
+        }
     }
 }
--- src/com/company/Joueur.java
+++ src/com/company/Joueur.java
@@ -1,5 +1,6 @@
 package com.company;
 
+import java.util.Scanner;
 import java.util.Vector;
 
 public class Joueur {
@@ -9,7 +10,54 @@ public class Joueur {
 		return bateaux;
 	}
 
-	public Joueur() {
+
+    private int[][] grille = new int[10][10];
+
+    public Joueur() {
+    }
+
+    public int lignebateau() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Veuillez saisir une ligne :");
+        int str = sc.nextInt();
+        System.out.println("Vous avez saisi : " + str);
+        return str;
+    }
+
+    public int colonebateau() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Veuillez saisir une colone :");
+        int str = sc.nextInt();
+        System.out.println("Vous avez saisi : " + str);
+        return str;
+    }
+
+    public boolean orienterbateau() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("il est horizontal ? true/false :");
+        String str2 = sc.next();
+        System.out.println("Vous avez saisi : " + str2);
+        boolean bool = false;
+        if (str2.equals("true")) {
+            bool = true;
+        }
+
+        return bool;
+    }
+
+    public void montrergrille() {
+        System.out.print("  0 1 2 3 4 5 6 7 8 9\n");
+
+        for(int i = 0; i < this.grille.length; ++i) {
+            System.out.print(i + " ");
+
+            for(int j = 0; j < this.grille[i].length; ++j) {
+                System.out.print(this.grille[i][j] + " ");
+            }
+
+            System.out.println();
+        }
+
     }
 
     public void ajouterbateau(Bateau b) {
